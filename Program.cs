8b4ba8675d538f8bdd2748bb2d985e5a27c6ba38
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-01 20:14:40 -0300

    Tarefa finalizada

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+Console.ForegroundColor = ConsoleColor.DarkMagenta;
+Console.WriteLine("Escreva seu nome completo");
+Console.ResetColor();
+
+string imput = Console.ReadLine() ?? string.Empty;
+
+
+string transformado = imput
+    .Replace("a", "4")
+    .Replace("e", "3")
+    .Replace("i", "1")
+    .Replace("o", "0")
+    .Replace("s", "5");
+
+Console.WriteLine($"Transformado: {transformado}");
+        Console.WriteLine("Pressione qualquer tecla para sair...");
+        Console.ReadKey();
+
